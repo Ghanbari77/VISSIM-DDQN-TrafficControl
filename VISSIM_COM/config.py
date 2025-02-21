8b0c1127e66958fe2e3c130401cbeaeb3c44029b
Mohammad Ghanbari <qanbarii.mohammad@gmail.com>
2025-02-21 16:04:39 -0500

    Create config.py

---
+++ VISSIM_COM/config.py
@@ -0,0 +1,13 @@
+# DQN Hyperparameters
+learning_rate = 0.001
+batch_size = 64
+max_memory_size = 15000
+gamma = 0.99
+
+# Exploration Parameters
+epsilon = 1.0
+epsilon_decay = 0.9995
+epsilon_min = 0.01
+
+# Simulation parameters
+Num_episodes = 200
