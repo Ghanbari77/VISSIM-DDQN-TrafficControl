534fe3bf14a0c204ef45aabc273323cfebd6235f
Mohammad Ghanbari <qanbarii.mohammad@gmail.com>
2025-02-21 17:18:46 -0500

    Update config.py

--- VISSIM_COM/config.py
+++ VISSIM_COM/config.py
@@ -19,3 +19,35 @@ epsilon_min = 0.01
 
 # Main Loop for Training
 Num_episodes = 200
+
+
+#Traffic Signal Control Parameters  
+# Dixie & 401
+offset_Dixie_401 = 129
+Min_green_SB_NB_Dixie_401 = 100
+Min_green_WB_Dixie_401 = 45
+End_of_Last_Cycle_Time_Dixie_401 = 0
+split_Dixie_401 = [Min_green_SB_NB_Dixie_401, Min_green_WB_Dixie_401]
+
+# Dixie & Shawson
+End_of_Last_Barrier_Time_Dixie_Shawson = 0
+offset_Dixie_Shawson = 1
+Start_time_Dixie_Shawson = 0
+spilits_Dixie_Shawson = [14, 87, 10, 87, 0, 52, 31, 23]
+result_Dixie_Shawson = Ring_Barrier(spilits_Dixie_Shawson)
+Ring1_Dixie_Shawson = True
+if offset_Dixie_Shawson >= result_Dixie_Shawson.x[0] + result_Dixie_Shawson.x[1]:
+    Ring1_Dixie_Shawson = False
+    offset_Dixie_Shawson = offset_Dixie_Shawson - result_Dixie_Shawson.x[0] - result_Dixie_Shawson.x[1]
+change_Dixie_Shawson = False
+
+# Dixie & Britannia
+End_of_Last_Barrier_Time_Dixie_Britannia = 0
+offset_Dixie_Britannia = 1
+Start_time_Dixie_Britannia = 0
+spilits_Dixie_Britannia = [14, 82, 11, 85, 0, 58, 21, 43]
+result_Dixie_Britannia = Ring_Barrier(spilits_Dixie_Britannia)
+Ring1_Dixie_Britannia = True
+if offset_Dixie_Britannia >= result_Dixie_Britannia.x[0] + result_Dixie_Britannia.x[1]:
+    Ring1_Dixie_Britannia = False
+    offset_Dixie_Britannia = offset_Dixie_Britannia - result_Dixie_Britannia.x[0] - result_Dixie_Britannia.x[1]
